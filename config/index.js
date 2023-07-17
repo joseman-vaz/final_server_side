d61e564b12f70e9e8cc8d3c9d61895aaaf7b0d21
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-07-17 16:56:55 +0200

    Update

--- config/index.js
+++ config/index.js
@@ -25,6 +25,9 @@ const configureMiddleware = (app) => {
   app.use(
     cors({
       origin: ["http://localhost:5173", process.env.ORIGIN],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
     })
   );
 
